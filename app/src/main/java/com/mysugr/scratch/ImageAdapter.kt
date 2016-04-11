7bf2e12433e11fdf91c2a1481c17a7a196762d11
Peter Messenger <peter.messenger@mysugr.com>
2016-04-12 00:46:03 +0200

    renaming file

--- app/src/main/java/com/mysugr/scratch/ImageAdapter.kt
+++ app/src/main/java/com/mysugr/scratch/ImageAdapter.kt
@@ -5,25 +5,25 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
 
-class ImageAdapter(val showAction: (url: String, finishedAction: (Bitmap?) -> Unit) -> Unit) : RecyclerView.Adapter<CardViewHolder>() {
+class ImageAdapter(val showAction: (url: String, finishedAction: (Bitmap?) -> Unit) -> Unit) : RecyclerView.Adapter<ImageViewHolder>() {
 
     var imageUrls: List<String> = arrayListOf()
 
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): CardViewHolder? {
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ImageViewHolder? {
         val cardView = LayoutInflater.from(parent?.context).inflate(R.layout.list_entry, parent, false)
-        return CardViewHolder(cardView)
+        return ImageViewHolder(cardView)
     }
 
     override fun getItemCount(): Int {
         return imageUrls.count();
     }
 
-    override fun onBindViewHolder(holder: CardViewHolder?, position: Int) {
+    override fun onBindViewHolder(holder: ImageViewHolder?, position: Int) {
         holder!!.reset()
         showAction(imageUrls[position], { bitmap -> setImage(holder, bitmap) });
     }
 
-    private fun setImage(holder: CardViewHolder, image: Bitmap?) {
+    private fun setImage(holder: ImageViewHolder, image: Bitmap?) {
         if (image == null) return
 
         holder.setImage(image)
--- app/src/main/java/com/mysugr/scratch/ImageViewHolder.kt
+++ app/src/main/java/com/mysugr/scratch/ImageViewHolder.kt
@@ -5,7 +5,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.widget.ImageView
 
-class CardViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
+class ImageViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
     private val imageView = itemView!!.findViewById(R.id.card_image) as ImageView
     private val defaultImage = imageView.drawable;
 
